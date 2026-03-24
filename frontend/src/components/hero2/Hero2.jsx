0d1987308b9ceb2a2e065ba9cd11e53acc715b5e
Vikas Sourashtriya <vikassourashtriya30@gmail.com>
2026-03-24 13:33:41 +0530

    change home page

--- frontend/src/components/hero2/Hero2.jsx
+++ frontend/src/components/hero2/Hero2.jsx
@@ -9,7 +9,7 @@ const Hero2 = () => {
             <h1 class="text-4xl md:text-5xl font-bold">Explore</h1>
             <h1 class="text-4xl md:text-5xl font-bold">the Wonders in</h1>
             <h1 class="text-4xl md:text-6xl font-bold text-[#41A4FF]">
-              Sri Lanka
+              India
             </h1>
             <p class="mt-4">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
